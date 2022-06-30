983a301610baae9b8b28027626911c88cf3bee6a
Kelen Vargas <kelenvargas.senac@gmail.com>
2022-06-30 19:59:46 -0300

    enviando uma parte do meu PI

---
+++ src/Modell/Cadastro.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modell;
+
+/**
+ *
+ * @author Kelen
+ */
+public class Cadastro {
+    private int Idusuario;
+    private String email, senha;
+    
+
+    public Cadastro() {
+    }
+
+    public Cadastro(int Idusuario, String email, String senha) {
+        this.Idusuario = Idusuario;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getIdusuario() {
+        return Idusuario;
+    }
+
+    public void setIdusuario(int Idusuario) {
+        this.Idusuario = Idusuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+        
+   
+    public void salvar(){
+        
+       
+            
+        }
+        
+    }
+    
+    
+    
+
+    
+
